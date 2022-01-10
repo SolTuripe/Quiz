5838a5f4474f2bfa65abb297337d0bcd211f1602
Sol <solcis.bio@gmail.com>
2022-01-10 20:43:58 +0100

    questions and answers

---
+++ data/data.js
@@ -0,0 +1,19 @@
+export const data = [
+  {
+    question: "Which of the following options is not a code editor?",
+    choices: ["vim", "vscode", "emacs", "word"],
+    answer: "word",
+  },
+
+  {
+    question: "Which programming language is not object oriented?",
+    choices: ["java", "haskell", "C++", "Python"],
+    answer: "haskell",
+  },
+
+  {
+    question: "Which programming language is not good for styling websites?",
+    choices: ["stylus", "less", "sass", "PostCSS", "NextCSS"],
+    answer: "PostCSS",
+  },
+];
