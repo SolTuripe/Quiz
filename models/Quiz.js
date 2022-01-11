2939d21676fe107c7989decb0b3d87f8686f8319
Sol <solcis.bio@gmail.com>
2022-01-11 18:48:05 +0100

    ts-check

--- models/Quiz.js
+++ models/Quiz.js
@@ -1,6 +1,25 @@
+@ts-check
+import { questions } from './Question.js'
+
 export class Quiz {
 
-    constructor(choices) {
-        this.choices = choices
+    questionIndex = 0
+    score = 0
+
+    constructor(questions) {
+        this.questions = questions
+    }
+
+    getQuestionIndex() {
+        return this.questions[this.questionIndex]
+    }
+
+    guess(answer) {
+
+        if (this.getQuestionIndex()) {
+            this.score++
+        }
+
+        this.questionIndex++
     }
 }
