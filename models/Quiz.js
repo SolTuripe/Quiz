5cf1a9b751626ca0c1131177fc12c7b1224d2ff3
Sol <solcis.bio@gmail.com>
2022-01-11 19:07:25 +0100

    isEnded function

--- models/Quiz.js
+++ models/Quiz.js
@@ -1,25 +1,41 @@
-@ts-check
-import { questions } from './Question.js'
+//@ts-check
+import { Question } from "./Question.js";
 
 export class Quiz {
+  questionIndex = 0;
+  score = 0;
 
-    questionIndex = 0
-    score = 0
+  /**
+   *
+   * @param {Question[]} questions
+   */
 
-    constructor(questions) {
-        this.questions = questions
+  constructor(questions) {
+    this.questions = questions;
+  }
+
+  /**
+   *
+   * @returns {Question} the question found
+   */
+
+  getQuestionIndex() {
+    return this.questions[this.questionIndex];
+  }
+
+  isEnded() {
+    return this.questions.length === this.questionIndex;
+  }
+
+  /**
+   *
+   * @param {string} answer some text
+   */
+
+  guess(answer) {
+    if (this.getQuestionIndex().correctAnswer(answer)) {
+      this.score++;
     }
-
-    getQuestionIndex() {
-        return this.questions[this.questionIndex]
-    }
-
-    guess(answer) {
-
-        if (this.getQuestionIndex()) {
-            this.score++
-        }
-
-        this.questionIndex++
-    }
+    this.questionIndex++;
+  }
 }
