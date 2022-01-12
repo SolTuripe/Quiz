3feebea24390b466c1d71dedcd3872214c3fefc6
Sol <solcis.bio@gmail.com>
2022-01-12 16:25:27 +0100

    showChoices

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import { questions } from "./data/questions.js";
-import { Quiz } from "./models/Quiz.js"
+import { Quiz } from "./models/Quiz.js";
+import { UI } from "./models/UI.js";
 
 function main() {
     const quiz = new Quiz(questions);
+    const ui = new UI();
 
-    quiz.guess('agasghsg')
+    ui.showQuestion(quiz.getQuestionIndex().text);
 }
 
 main();
--- models/UI.js
+++ models/UI.js
@@ -1,13 +1,18 @@
 export class UI {
-    constructor() {}
+  constructor() {}
 
+  /**
+   *
+   * @param {string} text question to render
+   */
+  showQuestion(text) {
+    const questionTitle = document.querySelector("#question");
+    questionTitle.innerText = text;
+  }
 
-    /**
-     * 
-     * @param {string} text question to render
-     */
-
-   showQuestion(text) {
-        const questionTitle = document.querySelector("#question")
-   }
+  /**
+   * 
+   * @param {string} choices the choices of the question
+   */
+  showChoices(choices)
 }
